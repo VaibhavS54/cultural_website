6715dcbcf632765bfa70f327b3ce482c59ff63d2
Sharvil Lade <sharvillade9917@gmail.com>
2024-04-01 01:36:50 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import './App.css';
-import Footer from "./footer/footer"
-import Nav from './navbar/Nav';
-import ClubSection from './Clubs/ClubSection';
-import Hero from './home/hero';
-import About from './about/about';
+import "./App.css";
+import Footer from "./footer/footer";
+import Nav from "./navbar/Nav";
+import ClubSection from "./Clubs/ClubSection";
+import Hero from "./home/hero";
+import About from "./about/about";
+import Gallery from "./gallery/Gallery";
 
 function App() {
   return (
-  <>
-  <Nav/>
-  
-    <Hero/>
-    <About/>
-   <ClubSection />
+    <>
+      <Nav />
 
-  
-  
-    <Footer/>
-    
-    
+      <Hero />
+      <About />
+      <ClubSection />
+      <Gallery />
+
+      <Footer />
     </>
   );
 }
---
+++ src/gallery/Gallery.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+
+import slide_image_1 from "../assests/photos/img_1.jpg";
+import slide_image_2 from "../assests/photos/img_1.jpg";
+import slide_image_3 from "../assests/photos/img_1.jpg";
+import slide_image_4 from "../assests/photos/img_1.jpg";
+import slide_image_5 from "../assests/photos/img_1.jpg";
+import slide_image_6 from "../assests/photos/img_1.jpg";
+import slide_image_7 from "../assests/photos/img_1.jpg";
+
+function Gallery() {
+  return (
+    <div className=" max-w-screen bg-slate-800">
+      <h1 className=" py-14 text-center font-bold text-4xl mb-11 ">
+        G A L L E R Y
+      </h1>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: ".swiper-pagination", clickable: true }}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Gallery;
