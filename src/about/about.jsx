ad6f9bc34c3911dbd150202d446e2f9efcb7a69d
sharvil-lade <sharvillade9917@gmail.com>
2024-06-24 12:06:09 +0530

    updated footer

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,42 +1,60 @@
-import React from 'react'
-import { Basic } from '../Clubs/Reveal'
+import React from "react";
+import { Basic } from "../Clubs/Reveal";
 
 function About() {
   return (
-    <div className="bg-black" id='about'>
-        <Basic>
-        <h1 className="  text-center text-3xl md:text-5xl lg:text-5xl text-3xl mt-20 mb-11 md:mt-32 md:mb-20  font-bold tracking-widest text-white text-left">ABOUT US</h1>
-        </Basic>
-        <div className="text-white text-xl text-left pl-24 pr-24">
-            <h3 className=" py-4 ">
-                <p>At Artificium, we believe in celebrating the vibrant tapestry of cultures that intertwine within our college community. Like a kaleidoscope,
-                we offer a mesmerizing array of events and activities that bring people together, fostering connections and friendships that last a lifetime.</p>
-          </h3>
+    <div className="bg-black" id="about">
+      <Basic>
+        <h1 className="  text-center text-3xl md:text-5xl lg:text-5xl text-3xl mt-20 mb-11 md:mt-32 md:mb-20  font-bold tracking-widest text-white text-left">
+          ABOUT US
+        </h1>
+      </Basic>
+      <div className="text-white text-xl text-left pl-24 pr-24">
+        <h3 className=" py-4 ">
+          <p>
+            At Artificium, we believe in celebrating the vibrant tapestry of
+            cultures that intertwine within our college community. Like a
+            kaleidoscope, we offer a mesmerizing array of events and activities
+            that bring people together, fostering connections and friendships
+            that last a lifetime.
+          </p>
+        </h3>
 
-            <div className='summary2'>
-                <h3 className=" py-4 ">
-                    <p>From the soothing embrace of Nebula's melodies to the exhilarating energy of our festive celebrations and grand events, Artificium is a
-                    melting pot of creativity and expression. Here, budding visionaries are encouraged to unleash their talents, enriching lives and creating
-                    unforgettable moments.</p>
-                </h3>
-            </div>
-            <div className='summary3'>
-                <h3 className=" py-4 ">
-                    <p>Our diverse array of clubs ensures that there's something for everyone. Whether you're a cinephile craving the magic of the silver screen
-                    with Cinemara, a music enthusiast looking to harmonize with Tarang-Sargam, or an aficionado of the arts seeking inspiration with
-                    Reflection, there's a club waiting to welcome you into our creative family.</p>
-                </h3>
-            </div>
-            <div className='summary4'>
-                <h3 className=" py-4 ">
-                    <p>But the magic of Artificium doesn't stop there. Gyrte, Mysteve, AnimeVerse, Expression, and Samwaad Club offer even more opportunities
-                    to explore, learn, and grow together.</p>
-                </h3>
-            </div>
+        <div className="summary2">
+          <h3 className=" py-4 ">
+            <p>
+              From the soothing embrace of Nebula's melodies to the exhilarating
+              energy of our festive celebrations and grand events, Artificium is
+              a melting pot of creativity and expression. Here, budding
+              visionaries are encouraged to unleash their talents, enriching
+              lives and creating unforgettable moments.
+            </p>
+          </h3>
         </div>
+        <div className="summary3">
+          <h3 className=" py-4 ">
+            <p>
+              Our diverse array of clubs ensures that there's something for
+              everyone. Whether you're a cinephile craving the magic of the
+              silver screen with Cinemara, a music enthusiast looking to
+              harmonize with Tarang-Sargam, or an aficionado of the arts seeking
+              inspiration with Reflection, there's a club waiting to welcome you
+              into our creative family.
+            </p>
+          </h3>
+        </div>
+        <div className="summary4">
+          <h3 className=" py-4 ">
+            <p>
+              But the magic of Artificium doesn't stop there. Gyrte, Mysteve,
+              AnimeVerse, Expression, and Samwaad Club offer even more
+              opportunities to explore, learn, and grow together.
+            </p>
+          </h3>
+        </div>
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default About
+export default About;
--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -1,51 +1,152 @@
-import React from 'react'
-import Itemscontainer from './itemscontainer'
+import React from "react";
+import fb from "./icons/icons8-facebook-f-24 (2).png";
+import insta from "./icons/icons8-instagram-24.png";
+import linkedin from "./icons/icons8-linkedin-24 (1).png";
+import twitter from "./icons/icons8-twitter-24.png";
+import { FaHome, FaEnvelope } from "react-icons/fa";
 
-import fb from "./icons/icons8-facebook-f-24 (2).png"
-import insta from "./icons/icons8-instagram-24.png"
-import linkedin from "./icons/icons8-linkedin-24 (1).png"
-import twitter from "./icons/icons8-twitter-24.png"
-
-
-
-
-const Footer=()=> {
+const Footer = () => {
   return (
-    <footer id='footer' className="relative object-bottom bottom-0 bg-gray-900 text-white sm:w-full
-    ">
-      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 py-7 ">
-      
-        <h1 className="lg:text-4xl text-4xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-4/5" ><span className="text-white font-rambla text-5xl italic leading-16 tracking-tighter cursor-default text-center 
-         hover:text-">Artificium</span></h1>
-        <div className='grid grid-cols-4 md:grid-cols-4 sm:grid-cols-4 lg:grid-cols-4 gap-6 mr-3 '>
-        <a href='https://www.facebook.com/' ><img src={fb} alt="fb" className="cursor-pointer "  /></a>
-        <a href='https://www.instagram.com/' ><img src={insta} alt="insta" className="cursor-pointer " /></a>
-        <img src={linkedin} alt="linkedin" className="cursor-pointer "/>
-        <img src={twitter} alt="twitter" className="cursor-pointer "/>
-</div>
-       {/*} <div class="social-container"> 
-      <h3>Social Follow</h3>
-      <a href="https://www.youtube.com/c/jamesqquick"
-        className="youtube social">
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </a>
-  </div>
-          <input type='text' placeholder='Enter name' className='text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 mb-4 py-2.5 rounded px-2 focus:outline-none'>
-          </input>
-          <button className='bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[Poppins] rounded-md text-white md:w-auto w-full'>
-            Request code
-  </button>*/}
-  
+    <footer
+      id="footer"
+      className="relative object-bottom bottom-0 bg-blue-900 text-white sm:w-full"
+    >
+      <div className="md:flex md:justify-between md:items-center sm:px-12 px-4 py-7">
+        <div className="grid grid-cols-4 gap-6 mb-6 md:mb-0">
+          <div>
+            <h2 className="font-bold text-lg mb-2 relative">
+              USEFUL LINKS
+              <div className="w-20 border-b-2 border-yellow-500 absolute left-0 bottom-0"></div>
+            </h2>
+            <ul>
+              <li>
+                <a href="#" className="hover:text-yellow-500">
+                  Link 1
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-yellow-500">
+                  Link 2
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-yellow-500">
+                  Link 3
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-yellow-500">
+                  Link 4
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-yellow-500">
+                  Link 5
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-yellow-500">
+                  Link 6
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg mb-2 relative">
+              LOCATION
+              <div className="w-20 border-b-2 border-yellow-500 absolute left-0 bottom-0"></div>
+            </h2>
+            <ul>
+              <li>
+                <a
+                  href="https://www.iiitbh.ac.in/how-reach"
+                  className="hover:text-yellow-500"
+                >
+                  HOW TO REACH
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.iiitbh.ac.in/accomodation"
+                  className="hover:text-yellow-500"
+                >
+                  HOTELS NEAR THE INSTITUTE
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg mb-2 relative">
+              CONTACT INFO
+              <div className="w-20 border-b-2 border-yellow-500 absolute left-0 bottom-0"></div>
+            </h2>
+            <p className="py-1">
+              Indian Institute of Information Technology Bhagalpur.
+            </p>
+            <p className="py-1">
+              <FaHome className="inline mr-2" /> Bhagalpur College of
+              Engineering Campus Sabour, Bhagalpur - 813210
+            </p>
+            <p className="py-1">
+              <FaEnvelope className="inline mr-2" />{" "}
+              <a
+                href="mailto:pro@iiitbh.ac.in"
+                className="hover:text-yellow-500"
+              >
+                pro@iiitbh.ac.in
+              </a>
+            </p>
+          </div>
+          <div>
+            <h2 className="font-bold text-lg mb-2 relative">
+              CONNECT WITH US
+              <div className="w-20 border-b-2 border-yellow-500 absolute left-0 bottom-0"></div>
+            </h2>
+            <div className="flex space-x-4">
+              <a href="https://www.facebook.com/iiitbh.ac.in">
+                <img
+                  src={fb}
+                  alt="fb"
+                  className="cursor-pointer hover:opacity-75"
+                />
+              </a>
+              <a href="https://www.instagram.com/iiit_bhagalpur_official">
+                <img
+                  src={insta}
+                  alt="insta"
+                  className="cursor-pointer hover:opacity-75"
+                />
+              </a>
+              <a href="https://www.linkedin.com/school/iiitbh">
+                <img
+                  src={linkedin}
+                  alt="linkedin"
+                  className="cursor-pointer hover:opacity-75"
+                />
+              </a>
+              <a href="https://www.twitter.com/iiitbhagalpur">
+                <img
+                  src={twitter}
+                  alt="twitter"
+                  className="cursor-pointer hover:opacity-75"
+                />
+              </a>
+            </div>
+            <div className="mt-4">
+              <h3 className="font-bold text-lg w-20 border-b-2 border-yellow-500">
+                VISITORS
+              </h3>
+              <p>Total Visitors: 1000</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <Itemscontainer></Itemscontainer>
-      <hr className='w-9/10 ml-4 mr-4 object-center'/>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-10 pt-2 text-gray-400 text-sm pb-8">
-        <span className='text-left ml-4'>All right reserved @2024, Artificium</span>
-      <span className='text-left mr-4 sm:text-right ml-4 lg:text-right ml-4'>Terms & conditions | Privacy Policy</span>
-      
+      <hr className="w-full mx-4" />
+      <div className="text-center p-4 text-white text-xl">
+        <span className="font-bold">All right reserved @2024, Artificium</span>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
