043398e92087f7fcdf1bbc2901f4bf5aec80fcb9
VaibhavS54 <vaibhav.2201190me@iiitbh.ac.in>
2024-07-04 17:16:02 +0530

    pages

--- src/gallery/Gallery.jsx
+++ src/gallery/Gallery.jsx
@@ -28,7 +28,7 @@ function Gallery() {
 
   return (
     <div className=" px-10 mb-32">
-      <div className="text-4xl font-bold text-center text-white">GALLERY</div>
+      <div className="text-5xl font-bold text-center text-white">GALLERY</div>
       <Swiper
         effect={"coverflow"}
         grabCursor={true}
